04d8ec6b05cb1be5d2e76a973fffc88b72fe779e
pejovic <milutinpejovic@gmail.com>
2019-03-06 13:56:27 +0100

    ...

--- R/design.r
+++ R/design.r
@@ -38,8 +38,8 @@ vb <- surveynet.xlsx(points = vb_points, observations = vb_obs, dest_crs = 3857)
 
 
 
-fix.params <- function(survey.net){
-  survey.net[[1]] %>% st_drop_geometry() %>% t() %>% as.data.frame(stringsAsFactors = FALSE) %>%
+fix.params <- function(net.points){
+  net.points %>% st_drop_geometry() %>% t() %>% as.data.frame(stringsAsFactors = FALSE) %>%
     rownames_to_column() %>%
     `colnames<-`(.[1,]) %>%
     .[-1,] %>%
@@ -50,54 +50,64 @@ fix.params <- function(survey.net){
 }
 
 
-Amat <- function(survey.net){
+Amat <- function(survey.net, units, axes = c("Easting", "Northing")){
+
   A_dir <- survey.net[[2]] %>% filter(direction) %>% st_coordinates() %>% as.data.frame() %>% mutate_at(vars(L1), funs(factor)) %>%
     split(., .$L1) %>%
-    lapply(., function(x) coef_p(pt1 = x[1, 1:2], pt2 = x[2, 1:2], pts = st_coordinates(survey.net[[1]][, 1:2]))) %>%
+    lapply(., function(x) coef_p(pt1 = x[1, 1:2], pt2 = x[2, 1:2], pts = st_coordinates(survey.net[[1]][, 1:2]), units = units, axes = axes)) %>%
     do.call(rbind, .)
 
   A_dist <- survey.net[[2]] %>% filter(distance) %>% st_coordinates() %>% as.data.frame() %>% mutate_at(vars(L1), funs(factor)) %>%
     split(., .$L1) %>%
-    lapply(., function(x) coef_d(pt1 = x[1, 1:2], pt2 = x[2, 1:2], pts = st_coordinates(survey.net[[1]][, 1:2]))) %>%
+    lapply(., function(x) coef_d(pt1 = x[1, 1:2], pt2 = x[2, 1:2], pts = st_coordinates(survey.net[[1]][, 1:2]), units = units, axes = axes)) %>%
     do.call(rbind, .)
 
   Z_mat <- survey.net[[2]] %>% filter(direction) %>%
     spread(key = from, value = direction, fill = FALSE) %>%
-    select(survey.net[[1]]$Name[!survey.net[[1]]$Point_object]) %>%
+    dplyr::select(survey.net[[1]]$Name[!survey.net[[1]]$Point_object]) %>%
     st_drop_geometry() %>%
     as.matrix()*1
 
-  fix <- fix.params(survey.net)
+  fix <- fix.params(net.points = survey.net[[1]])
 
   rest_mat <- matrix(0, nrow = dim(A_dist)[1], ncol = dim(Z_mat)[2])
 
   A <- cbind(rbind(A_dir, A_dist)[, !fix], rbind(Z_mat, rest_mat))
 
-  colnames(A) <- c(paste(rep(survey.net[[1]]$Name, each = 2), rep(c("x", "y"), length(survey.net[[1]]$Name)), sep = "_")[!fix], paste(colnames(Z_mat), "z", sep = "_"))
+  if(("Easting" == axes)[1]) {sufix <- c("dE", "dN")} else {sufix <- c("dN", "dE")}
+  colnames(A) <- c(paste(rep(survey.net[[1]]$Name[], each = 2), rep(sufix, length(survey.net[[1]]$Name)), sep = "_")[!fix], paste(colnames(Z_mat), "z", sep = "_"))
   return(A)
 }
 
-Amat(survey.net = vb)
+Amat(survey.net = ib, units = "mm")
 
 
 ib_points <- readxl::read_xlsx(path = here::here("Data/Input/Without_observations/xlsx/IB.xlsx"), sheet = "Points", col_types = c("text", "numeric", "numeric", "logical", "logical", "logical"))
 ib_obs <- readxl::read_xlsx(path = here::here("Data/Input/Without_observations/xlsx/IB.xlsx"), sheet = "Observations", col_types = c("numeric", "text", "text", "logical", "logical", "numeric", "numeric"))
 ib <- surveynet.xlsx(points = ib_points, observations = ib_obs, dest_crs = 3857)
 
-Amat(survey.net = ib)
+Amat(survey.net = ib, units = "mm")
 
 # Weights matrix
 Wmat <- function(survey.net, apriori = 1){
   #TODO: Omoguciti zadavanje i drugih kovariacionih formi izmedju merenja.
   obs.data <- survey.net[[2]] %>% st_drop_geometry() %>%
     gather(key = type, value = standard, -c(id, from, to, distance, direction)) %>%
-    select(from, to, standard)
+    dplyr::select(from, to, standard)
   return(diag(apriori^2/obs.data$standard^2))
 }
 
 
-design.snet <- function(survey.net){
-  A <- Amat(survey.net)
+design.snet <- function(survey.net, apriori = 1, result.units = list("mm", "cm", "m"), axes = c("Easting", "Northing")){
+
+  # Check which points are used for measurements
+  used.points <- unique(do.call(c, survey.net[[2]][, c("from", "to")] %>% st_drop_geometry()))
+  used.points.ind <- which(survey.net[[1]]$Name %in% used.points)
+  survey.net[[1]] <- survey.net[[1]][used.points.ind, ]
+  #
+
+  units <- result.units[[1]]
+  A <- Amat(survey.net, units = units, axes = axes)
   W <- Wmat(survey.net)
   N <- crossprod(A, W) %*% A
   Qx <- tryCatch(
@@ -105,7 +115,7 @@ design.snet <- function(survey.net){
       x = Qx = solve(N)
     },
     error = function(e) {
-      x = Qx = matlib::Ginv(N)
+      x = Qx = MASS::ginv(N)
     })
   colnames(Qx) <- colnames(N)
   rownames(Qx) <- rownames(N)
@@ -117,6 +127,16 @@ design.snet <- function(survey.net){
   return(design)
 }
 
-design.snet(survey.net =  ib)
+# Ne radi dobro inverziju!!! Matrica nije simetricna!
+
+ib[[1]] <- filter(ib[[1]], Name != "T4")
+dd <- design.snet(survey.net =  ib, result.units = "mm")
+Qx <- dd$Qx
+nn = dim(ib[[1]])[1] # TODO: ovde treba isto uzeti used.points samo. Odnosno biti siguran koliko ima tacaka za koje se racuna elipsa
+fix = ib[[1]] %>% st_drop_geometry() %>% dplyr::select(FIX_X, FIX_Y) == FALSE
+fix <- fix*1
+
+Qxy(Qx, n = nn, fixd = fix)
+
 
 
